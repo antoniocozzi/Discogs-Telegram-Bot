fa76d4bf50f24c4a9815f2761c9b8ee75c3eaeeb
Antonio Cozzi <antoniocozzi2@gmail.com>
2018-02-04 00:21:50 +0100

    Added waitForRequest functionality which expects the Discogs username

--- controllers/discogs.js
+++ controllers/discogs.js
@@ -6,33 +6,50 @@ class DiscogsController extends Telegram.TelegramBaseController {
      sayHelloHandler($) {
           let db = new Discogs().database();
           let col = new Discogs().user().collection();
-          var username = $.message.text.split(' ').slice(1).join(' ');
-          col.getReleases(username, 0, {
-               page: 1,
-               per_page: 10
-          }, (err, data) => {
-               // $.sendMessage(JSON.stringify(data))
-               let releases = data.releases
-               console.log(releases)
-               for (var release in releases) {
-                    let title = releases[release].basic_information.title
-                    let artist = releases[release].basic_information.artists[0].name || ''
-                    let year = releases[release].basic_information.year
-                    let coverImg = releases[release].basic_information.cover_image
-                    let releaseAdditionalInfos = releases[release].basic_information.artists[0].resource_url
-                    // let vinylLink = JSON.parse(releaseAdditionalInfos)
-                    db.getRelease(releases[release].id, (err, data) => {
-                         let styles = data.styles;
-                         $.sendMessage(
-                              `Title: ${title}\nArtist: ${artist}\nYear: ${year}\nGenre: ${this.cycleStyles(styles)}\n
-                              ${coverImg}`);
-                    })
+          var username;
+          $.sendMessage('Send me your Discogs username')
+          $.waitForRequest
+               .then($ => {
+                    username = $.message.text
+                    col.getReleases(username, 0, {
+                         page: 1,
+                         per_page: 10
+                    }, (err, data) => {
+                         // $.sendMessage(JSON.stringify(data))
+                         let releases = data.releases
+                         for (var release in releases) {
+                              let title = releases[release].basic_information.title
+                              let artist = releases[release].basic_information.artists[0].name || ''
+                              let year = releases[release].basic_information.year
+                              let coverImg = releases[release].basic_information.cover_image
+                              let releaseAdditionalInfos = releases[release].basic_information.artists[0].resource_url
+                              let label = releases[release].basic_information.labels[0].name;
+                              let categoryNumber = releases[release].basic_information.labels[0].catno;
+                              // let vinylLink = JSON.parse(releaseAdditionalInfos)
+                              db.getRelease(releases[release].id, (err, data) => {
+                                   let styles = data.styles;
+                                   $.sendMessage(
+                                        `Title: ${title}\nArtist: ${artist}\nYear: ${year}\nLabel: ${label}\nCategory Name: ${categoryNumber}\nGenres: ${this.cycleStyles(styles)}\n
+                                        ${coverImg}`);
 
-               }
-          });
+                                   // $.runInlineMenu({
+                                   //      layout: 1,
+                                   //      method: 'sendMessage',
+                                   //      params: [''],
+                                   //      menu: [{
+                                   //           text: 'Show more', //text of the button
+                                   //           callback: (callbackQuery, message) => { //to your callback will be passed callbackQuery and response from method
+                                   //                console.log('Show more')
+                                   //           }
+                                   //      }]
+                                   // })
+                              })
+                         }
+                    });
+               })
      }
 
-     cycleStyles(styles = []){
+     cycleStyles(styles = []) {
           let styleStr = ''
           for (let style of styles) {
                styleStr += style + ', '
