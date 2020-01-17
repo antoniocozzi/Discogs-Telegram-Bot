159321184cf3cf8a71b7407ce4a6ca61e670844d
Antonio <antonio.cozzi@alten.it>
2020-01-17 22:22:47 +0100

    First attempt to search with new libs

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-// 'use strict'
-
-// const Telegram = require('telegram-node-bot'),
-// 	discogsTelegramBot = new Telegram.Telegram('463129846:AAGzPrtx-syuXbe8cc1wVbJLaeWhgocfcjc', {
-// 		workers: 1,
-// 	})
-
-// const DiscogsController = require('./controllers/discogs'),
-// 	OtherwhiseController = require('./controllers/otherwise')
-// const discogsController = new DiscogsController()
-
-// discogsTelegramBot.router
-// 	.when(new Telegram.TextCommand('/hello', 'sayHelloCommand'), discogsController)
-// 	.when(new Telegram.TextCommand('/search', 'searchCommand'), discogsController)
-//  .when(new Telegram.TextCommand('/username', 'usernameCommand'), discogsController)
-// 	.otherwise(new OtherwhiseController())
-
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
+const Markup = require('telegraf/Markup')
+const axios = require('axios');
 
 const telegramToken = '463129846:AAGzPrtx-syuXbe8cc1wVbJLaeWhgocfcjc';
+const discogsToken = 'znRRTXbZvhkJWWIDSeBmXftNwHTvTvvhRkFPapit';
 
 const bot = new Telegraf(telegramToken)
-bot.command('hello', (ctx) => ctx.reply('Send me your Discogs username'))
-bot.launch()
+
+bot.command("search", ctx => {
+    return ctx.replyWithMarkdown(`Enter something to search.`);
+})
+
+bot.on('text', ctx => {
+    const query = ctx.message.text;
+    let nextPage = 1;
+    const url = `https://api.discogs.com/database/search?q=${query}&page=${nextPage}&per_page=15&token=${discogsToken}`;
+
+    axios.get(url)
+        .then(response => {
+            response.data.results.map(item => {
+                if (item.type === "release")
+                    return ctx.replyWithPhoto(item.thumb, {
+                        caption: `💿 ${item.title}\n`
+                    })
+            })
+
+            /*
+                This piece below is pure madness 🤨
+            */
+
+            // nextPage++;
+            // return ctx.reply(
+            //     query,
+            //     Markup.inlineKeyboard([
+            //         Markup.callbackButton('➡️ Next page', nextPage),
+            //     ]).extra(),
+            // );
+        })
+        .catch(e => {
+            console.log(e)
+        })
+})
+
+bot.startPolling()
